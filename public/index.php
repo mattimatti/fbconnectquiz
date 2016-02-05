606217f7920fb6a4e626deb310bea611f252375b
mattimatti <mmonti@gmail.com>
2016-02-05 15:33:27 +0100

    asdasd

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ session_start();
 // Instantiate the app
 $settings = require __DIR__ . '/../src/production.php';
 
-print_r($settings['view']);
+print_r($settings);
 exit();
 
 $app = new \Slim\App($settings);
