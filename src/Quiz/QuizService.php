50b816e5149cf80b313ac6a5c79a48e19b5eafc1
mattimatti <mmonti@gmail.com>
2016-02-09 14:26:31 +0000

    Fix quiz service

--- src/Quiz/QuizService.php
+++ src/Quiz/QuizService.php
@@ -30,6 +30,7 @@ class QuizService
         $answer = R::xdispense(ANSWER);
         $answer->title = 'Han Solo';
         $answer->message = "You have a strong and determined personality, but beneath your tough exterior you have a loving heart and an inner bravery that will help you through the tough times. You learn from your mistakes and always stay true to yourself!";
+        $answer->sharemessage = "You have a strong and determined personality, but beneath your tough exterior you have a loving heart and an inner bravery that will help you through the tough times. You learn from your mistakes and always stay true to yourself!";
         $answer->image = 'A';
         $answer->{QUIZ} = $quiz;
         $answer->{QUESTION} = $question;
@@ -38,6 +39,7 @@ class QuizService
         $answer = R::xdispense(ANSWER);
         $answer->title = 'Luke Skywalker';
         $answer->message = "You're courageous, eager for adventure and an all-round hero. Sometimes you find it hard to control your emotions, but you always have the happiness and wellbeing of others at heart!";
+        $answer->sharemessage = "You're courageous, eager for adventure and an all-round hero. Sometimes you find it hard to control your emotions, but you always have the happiness and wellbeing of others at heart!";
         $answer->image = 'B';
         $answer->{QUIZ} = $quiz;
         $answer->{QUESTION} = $question;
@@ -47,6 +49,7 @@ class QuizService
         $answer->title = 'Princess Leia';
         $answer->image = 'C';
         $answer->message = "You're level-headed, courageous, and with a sharp-tongued wit surpassed only by your beauty. You've been through some tough times but always come out stronger in the end!";
+        $answer->sharemessage = "You're level-headed, courageous, and with a sharp-tongued wit surpassed only by your beauty. You've been through some tough times but always come out stronger in the end!";
         $answer->{QUIZ} = $quiz;
         $answer->{QUESTION} = $question;
         R::store($answer);
@@ -55,6 +58,7 @@ class QuizService
         $answer->title = 'Chewbacca';
         $answer->image = 'D';
         $answer->message = "You may look tough, and even scary to some people, but deep down you're a big softie. You're loyal, affectionate and humble - but if you think something isn't fair you're not afraid to say so! ";
+        $answer->sharemessage = "You may look tough, and even scary to some people, but deep down you're a big softie. You're loyal, affectionate and humble - but if you think something isn't fair you're not afraid to say so! ";
         $answer->{QUIZ} = $quiz;
         $answer->{QUESTION} = $question;
         R::store($answer);
@@ -63,6 +67,7 @@ class QuizService
         $answer->title = 'Finn';
         $answer->image = 'E';
         $answer->message = "Despite what life has thrown at you, you have a good heart and true empathy for other people. You are brave, intelligent and with the strength to face down whatever life throws at you!";
+        $answer->sharemessage = "Despite what life has thrown at you, you have a good heart and true empathy for other people. You are brave, intelligent and with the strength to face down whatever life throws at you!";
         $answer->{QUIZ} = $quiz;
         $answer->{QUESTION} = $question;
         R::store($answer);
@@ -71,6 +76,7 @@ class QuizService
         $answer->title = 'Rey';
         $answer->image = 'F';
         $answer->message = "You have a heart full of generosity and a desire to help others, often putting their needs before your own. You have a great imagination that sets you apart from the majority of others!";
+        $answer->sharemessage = "You have a heart full of generosity and a desire to help others, often putting their needs before your own. You have a great imagination that sets you apart from the majority of others!";
         $answer->{QUIZ} = $quiz;
         $answer->{QUESTION} = $question;
         R::store($answer);
