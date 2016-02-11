2dc134691795ac5997a17897db160bb562c29739
mattimatti <mmonti@gmail.com>
2016-02-11 17:36:03 +0000

    ok privacy

--- src/routes.php
+++ src/routes.php
@@ -7,9 +7,9 @@ $app->get('/login', 'App\Controller\AuthController:login')->setName('login');
 
 
 
+$app->get('/privacy', 'App\Controller\QuizController:privacy')->setName('privacy');
 $app->get('/install', 'App\Controller\QuizController:install')->setName('install');
 
 $app->get('/shared/{id}', 'App\Controller\QuizController:share')->setName('share');
 $app->get('/', 'App\Controller\QuizController:index')->setName('home');
-$app->get('/', 'App\Controller\QuizController:privacy')->setName('privacy');
 $app->post('/', 'App\Controller\QuizController:submit');
