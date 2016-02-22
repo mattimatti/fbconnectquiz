b8ac1db63d0928c63c70d6b7c761ff633f02d4f6
mattimatti <mmonti@gmail.com>
2016-02-22 12:13:42 +0000

    fixed login redirect

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -90,7 +90,7 @@ final class QuizController
         if (isset($_POST['selection'])) {
             
             if (! $this->captateUser($response)) {
-                return $response->withRedirect($this->router->pathFor('home'));
+                return $response->withRedirect($this->router->pathFor('login'));
             }
             
             $this->logger->debug('evaluate user selection : ' . $selected);
@@ -128,7 +128,7 @@ final class QuizController
     public function index(Request $request, Response $response, $args)
     {
         if (! $this->captateUser()) {
-            return $response->withRedirect($this->router->pathFor('home'));
+            return $response->withRedirect($this->router->pathFor('login'));
         }
         
         $this->logger->debug('render quiz page');
